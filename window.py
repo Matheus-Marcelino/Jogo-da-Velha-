15f52ad069d76a14f6796a22a4bc03714e007e80
Matheus@DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-12-27 12:16:36 -0300

    Iniciando os modulos com as classes principais

---
+++ window.py
@@ -0,0 +1,25 @@
+"""Auxiliar visual e criação do board"""
+from os import system, name
+from time import sleep
+from colorama import init
+
+class Colors:
+    """Cores para os textos"""
+    init()
+    end = '\033[m'
+    yellow = '\033[1;33m'
+    red = '\033[1;91m'
+    green = '\033[1;92m'
+    pink = '\033[1;35m'
+    blue = '\033[1;34m'
+    white = '\033[1m'
+    underline = '\033[4m'
+
+
+class Board:
+    """Metodos para axulixar o board"""
+    def __init__(self) -> None:
+        self.tela: list = [['', '', ''],
+                           ['', '', ''],
+                           ['', '', '']]
+
