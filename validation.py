b9cf491fb7d15edd475babfbaac62ded7884ad74
Matheus@DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-12-27 12:23:59 -0300

    metodo para conferir jogada

--- validation.py
+++ validation.py
@@ -5,4 +5,26 @@ from window import Board, Colors
 
 class Validation():
     """Validação de dados"""
-    pass
+    def __init__(self) -> None:
+        self.__board = Board()
+
+    def __validation(self, tela: list, jogada: int, player: int, **player_esc) -> list:
+        """Verifica se o local está vazio ou preenchido"""
+        def verification(inicio: int, fim: int, key: int) -> (list | None):
+            for indice, contador in enumerate(range(inicio, fim+1)):
+                if jogada == contador:
+                    if tela[key][indice] != '':
+                        print(Colors.red +'este lugar já está '
+                              'ocupado, escolha outro'+ Colors.end, '\n')
+                        sleep(1.5)
+                        return self.player_move(tela, player, player1_esc=player_esc['player1_esc'],
+                                                              player2_esc=player_esc['player2_esc'])
+                    match player:
+                        case 0:
+                            return self.__board.insert(tela,{'key1': key,
+                                                             'key2': indice,
+                                                             'token':player_esc['player1_esc']})
+                        case 1:
+                            return self.__board.insert(tela,{'key1': key,
+                                                             'key2': indice,
+                                                             'token':player_esc['player2_esc']})
