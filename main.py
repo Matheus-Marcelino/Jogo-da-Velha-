dcd11b3db9da378df289b3bec85cbd5f896ee65b
Matheus@DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-12-27 12:28:02 -0300

    funções de escrita

--- main.py
+++ main.py
@@ -5,3 +5,22 @@ from validation import Validation
 from window import Board, Colors
 
 
+def write_winner_round(text: str, space: int, break_spc: int, delay: int) -> None:
+    """Escreve de forma formatada o vencedor"""
+    board.clear_terminal()
+    board.header()
+    print(' ' * space, text, '\n'*break_spc)
+    board.show(tela)
+    board.clear_board(tela)
+    sleep(delay)
+    board.clear_terminal()
+
+
+def write_match_winner(text: str, space: int, delay: int) -> None:
+    """Escreve quem foi o ganhador geral"""
+    board.clear_terminal()
+    board.header()
+    print(' ' * space, text)
+    sleep(delay)
+
+
